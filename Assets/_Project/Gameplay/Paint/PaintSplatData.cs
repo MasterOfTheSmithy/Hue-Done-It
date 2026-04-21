b65bb1103ad2951e4f4259b43ec49847482bc145
MasterOfTheSmithy <lightenupmyday123@gmail.com>
2026-04-21 08:05:50 -0400

    Reconcile merged paint splat schema

--- Assets/_Project/Gameplay/Paint/PaintSplatData.cs
+++ Assets/_Project/Gameplay/Paint/PaintSplatData.cs
@@ -1,18 +1,29 @@
+// File: Assets/_Project/Gameplay/Paint/PaintSplatData.cs
 using UnityEngine;
 
 namespace HueDoneIt.Gameplay.Paint
 {
     public struct PaintSplatData
     {
-        public PaintEventKind EventKind;
-        public PaintSplatType SplatType;
-        public PaintSplatPermanence Permanence;
         public Vector3 Position;
         public Vector3 Normal;
         public float Radius;
         public float Intensity;
+        public Color Color;
+
+        public PaintEventKind EventKind;
+        public PaintSplatType SplatType;
+        public PaintSplatPermanence Permanence;
+
         public float ForceMagnitude;
         public Vector3 VelocityDirection;
+        public Vector3 TangentDirection;
+
+        // Keep both during merge reconciliation.
         public int PatternIndex;
+        public int PatternSeed;
+
+        public float StretchAmount;
+        public float RotationDegrees;
     }
 }
--- Assets/_Project/Gameplay/Paint/PaintSplatType.cs
+++ Assets/_Project/Gameplay/Paint/PaintSplatType.cs
@@ -1,15 +1,22 @@
+// File: Assets/_Project/Gameplay/Paint/PaintSplatType.cs
 namespace HueDoneIt.Gameplay.Paint
 {
     public enum PaintSplatType : byte
     {
-        Footstep = 0,
-        Landing = 1,
-        WallImpact = 2,
-        Punch = 3,
-        TaskInteract = 4,
-        RagdollImpact = 5,
-        ThrownObject = 6,
-        Flood = 7,
-        Generic = 8
+        Generic = 0,
+
+        Default = 1,
+        Footstep = 2,
+        MoveSmear = 3,
+        Landing = 4,
+        WallImpact = 5,
+        WallScrape = 6,
+        WallLaunchBurst = 7,
+        Punch = 8,
+        HeavyImpact = 9,
+        RagdollImpact = 10,
+        TaskInteract = 11,
+        ThrownObject = 12,
+        Flood = 13
     }
 }
